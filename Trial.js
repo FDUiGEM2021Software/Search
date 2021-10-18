cb0122a529af5546b10c78e157de6d98b5f5c065
TomGhostSmith <TomGhostSmith@gmail.com>
2021-10-19 02:16:18 +0800

    trial 1: link html

--- Trial.js
+++ Trial.js
@@ -1,12 +1,37 @@
-window.onload = function() {
-    var url = "main.json" /*json文件url*/
-    var request = new XMLHttpRequest();
-    request.open("get", url); /*设置请求方法与路径*/
-    request.send(null); /*不发送数据到服务器*/
-    request.onload = function() { /*XHR对象获取到返回信息后执行*/
-        if (request.status == 200) { /*返回状态为200，即为数据获取成功*/
-            var json = JSON.parse(request.responseText);
-            console.log(json);
+// window.onload = function() {
+//     var url = "main.json" /*json文件url*/
+//     var request = new XMLHttpRequest();
+//     request.open("get", url); /*设置请求方法与路径*/
+//     request.send(null); /*不发送数据到服务器*/
+//     request.onload = function() { /*XHR对象获取到返回信息后执行*/
+//         if (request.status == 200) { /*返回状态为200，即为数据获取成功*/
+//             var json = JSON.parse(request.responseText);
+//             console.log(json);
+//         }
+//     }
+// }
+
+// function ModifyFile() {
+//     var ofs = fso.CreateTextFile("text.txt", true);
+//     ofs.WriteLine("123");
+//     ofs.Close();
+// }
+
+// ModifyFile();
+
+function Search() {
+    $.ajax({
+        type: "get",
+        // url: "Trial.py",
+        // url: "Trial.py\hello",
+        url: "\hello",
+        data: { "info": "123" },
+        datatype: "json",
+        success: function(response) {
+            var output = response;
+            alert(output);
         }
-    }
+    })
 }
+
+Search()
--- Trial.py
+++ Trial.py
@@ -1,11 +1,55 @@
-class A:
-    def __init__(self):
-        self.a = "number"
-        self.b = True
-        self.c = 123456
-def main():
-    import json
-    a = A()
-    print (json.dumps(a, default=lambda obj: obj.__dict__, sort_keys=True, indent=4))
+# class A:
+#     def __init__(self):
+#         self.a = "number"
+#         self.b = True
+#         self.c = 123456
+# def main():
+#     import json
+#     a = A()
+#     print (json.dumps(a, default=lambda obj: obj.__dict__, sort_keys=True, indent=4))
 
-main()
+# main()
+
+
+# import eel
+# @eel.expose
+# def test_print(str):
+    
+#     print("进来了",str)
+#     test_process(str)  #python 中的处理过程，再调用js中的函数返回
+    
+    
+# def test_process(str):
+#     str2 = str+'已处理'
+#     eel.system_sends_message2(str2)  # 调用js文件里的函数，将结果返回去
+
+# from django.http import JsonResponse
+# # import urllib.response
+
+# def Try(request):
+#     # return "a"
+#     HttpResponse(request)
+
+
+    
+from flask import Flask, jsonify, request, render_template
+app = Flask(__name__)
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+
+    # POST request
+    if request.method == 'POST':
+        print('Incoming..')
+        print(request.get_json())  # parse as JSON
+        return 'OK', 200
+
+    # GET request
+    else:
+        message = {'greeting':'Hello from Flask!'}
+        return jsonify(message)  # serialize and use JSON headers
+
+@app.route('/test')
+def test_page():
+    # look inside `templates` and serve `index.html`
+    return render_template('index.html')
